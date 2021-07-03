c807d61fd0bb9814306352eb1658e064a839289d
jack <jacknajarro1@gmail.com>
2021-07-03 13:25:53 -0500

    empezando a crear CRUD

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ server.listen(puerto,()=>{
 //asi usamos el modulo express
 const express=require('express')
 const app=express()
+const bodyParser=require('body-parser')
 
 //Q BUSQUE UN PUERTO EN HEROKU Y SI NO EXISTE  QUE NOS DESIGNE AL 3000s
 const port=process.env.PORT || 3000;
@@ -79,7 +80,7 @@ app.use((req,res,next)=>{
         titulo:'perdimos señal',
         descripcion:'Estamooos callenndooo'
     })
-})
+}) 
 
 
 app.listen(port,()=>{
--- router/Mascotas.js
+++ router/Mascotas.js
@@ -28,4 +28,5 @@ router.get('/',async (req,res)=>{
     })*/
 })
 
+
 module.exports=router;
